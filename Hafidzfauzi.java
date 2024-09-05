08c637a3fd258e4c4c4b053c6f509e986790e96b
Fidz-Faz <hafidzfauzi078gmail.com>
2024-09-05 14:14:58 +0700

    ini adalah project pertamaku

---
+++ Hafidzfauzi.java
@@ -0,0 +1,144 @@
+public class Hafidzfauzi {
+    public static void main(String[] args) {
+
+        // char E = 'D';
+        // char C = 'Z';
+        // char T = 'A';
+
+        // System.out.println(E);
+        // System.out.println(C);
+        // System.out.println(T);
+
+        // // boolean
+        // boolean benar = true;
+        // boolean salah = false;
+
+        // System.out.println(benar);
+        // System.out.println(salah);
+        // // string
+        // String firstname = "dzaki";
+        // String lastname = "rafif";
+        // String fullname = firstname +""+ lastname;
+
+        // System.out.println(firstname);
+        // System.out.println(lastname);
+        // System.out.println(fullname);
+        // // varibel
+        // String nama ;
+        // nama = "dzaki";
+        // int umur = 17;
+        // String tinggal = "sudiang";
+
+        // System.out.println(nama);
+        // System.out.println(umur);
+        // System.out.println(tinggal);
+// String[] hafidzArray;
+        // hafidzArray = new String[3];
+
+        // hafidzArray[0] = "ganteng";
+        // hafidzArray[1] = "baik";
+        // hafidzArray[2] = "kalem";
+
+        // System.out.println(hafidzArray[0]);
+        // System.out.println(hafidzArray[1]);
+        // System.out.println(hafidzArray[2]);
+
+
+        // String[] namaNama = {
+        //     "dzaki","slebew", "gg"
+        // };
+
+        // int[] arrayint = new int[]{
+        //     1,2,3,4,5,6,7
+        // };
+
+        // long[] arraylong = {
+        //     10l,20l,30l        };
+
+
+        //     System.out.println(arrayint[2]);
+
+
+
+            // long[] arraylong = {
+            //     1000l,2000l,300000l
+            // };
+
+            // System.out.println(arraylong. length);
+
+
+
+            // int c = 100;
+            // int a = -50;
+            // int b = 10;
+
+            // System.out.println(c + a - b / b);
+
+            // c -=  30;
+            // System.out.println(c);
+
+            // int a = +100;
+            // int e= -10;
+            // int d = 1000;
+
+            // d++;
+            // System.out.println(d);
+
+        //     var nilai = 80;
+        //     var absen = 60;
+
+        //  if (nilai>= 80 && absen>= 80 ) {
+        //     System.out.println("Nilai Anda A");
+        //  } else if (nilai >= 70 && absen >= 70) {
+        //     System.out.println("Nilai Anda B");
+        //  } else if (nilai >= 60 && absen >= 60) {
+        //     System.out.println("Nilai Anda C");
+        //  } else if (nilai >= 50 && absen >= 50) {
+        //     System.out.println("Nilai Anda D");
+        //  } else if (nilai >= 40 && absen >= 40) {
+        //     System.out.println("Nilai Anda E");
+        //  } else if (nilai >= 30 && absen >= 30) {
+        //     System.out.println("Nilai Anda F");
+        //  } else if (nilai >= 20 && absen >= 20) {
+        //     System.out.println("Nilai Anda G");
+        //  } else if (nilai >= 10 && absen >= 10) {
+        //     System.out.println("Nilai Anda H");
+        //  } else {System.out.println("Tidak ada nilai");}
+
+
+
+         var orang = "Kelompok 5" ;
+
+         if (orang == "Kelompok 1" ) {
+            System.out.println("Afgan , Ananda , Uways");
+         } else if (orang == "Kelompok 2") {
+            System.out.println("Fathi, hafidz, Rafif");
+         }  else if (orang == "Kelompok 3") {
+            System.out.println("Fakhri, Shuhaib,Syatir");
+         }  else if (orang == "Kelompok 4") {
+            System.out.println("Arkana, aksa ,oji (aksa sudah tidak ada)");
+         }  else {System.out.println("tidak ada gengac");} 
+
+
+         
+         
+
+
+
+
+        
+
+
+
+
+
+        
+
+
+
+
+
+
+
+    }
+}
